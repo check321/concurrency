621b17a0ee28b13b73a6ac70cc7264e94ccd4c63
check321 <check321@aliyun.com>
2018-03-26 18:08:24 +0800

    新增同步/并发容器测试

---
+++ src/main/java/net/check321/concurrency/clazz/container/concurrency/ArrayListTest.java
@@ -0,0 +1,59 @@
+package net.check321.concurrency.clazz.container.concurrency;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.*;
+
+/**
+ * @auther:fyang
+ * @date: 2018/3/26
+ * @description: ArrayList -> Vector -> CopyOnWriteArrayList 并发操作测试
+ */
+@Slf4j
+public class ArrayListTest {
+
+    // 并发执行线程数
+    public static int threadCount = 100;
+
+    // 执行总次数
+    public static int exeCount = 10000;
+
+    // unsafe container
+//      public static List<Integer> container = new ArrayList<>();
+    public static List<Integer> container = new Vector<>();
+    // public static List<Integer> container = new CopyOnWriteArrayList<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        // thread pool
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Semaphore semaphore = new Semaphore(threadCount);
+        CountDownLatch countDownLatch = new CountDownLatch(exeCount);
+
+        for (int i = 0; i < exeCount; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    counting();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    log.error("semaphore error: ", e);
+                }
+                countDownLatch.countDown();
+            });
+
+        }
+
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("{} container size: {}", container.getClass().getName(), container.size());
+    }
+
+
+    static void counting() {
+        // +1
+        container.add(1);
+    }
+}
---
+++ src/main/java/net/check321/concurrency/clazz/container/sync/SyncContainer.java
@@ -0,0 +1,54 @@
+package net.check321.concurrency.clazz.container.sync;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Slf4j
+/**
+  * @auther:fyang
+  * @date: 2018/3/26
+  * @description: ArrayList、Vector Exception in thread "Thread-0" java.util.ConcurrentModificationException
+ *                CopyOnWriteArrayList
+ */
+public class SyncContainer {
+
+    static List<Integer> container = new Vector<>();
+
+    public static void main(String[] args) {
+        init();
+
+        // 遍历元素
+        new Thread(() -> {
+            for (Integer item : container) {
+                log.info("item: {}",item);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    log.error("InterruptedException :", e);
+                }
+            }
+        }).start();
+
+        // 删除一个元素
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                log.error("InterruptedException :", e);
+            }
+            container.remove(2);
+        }).start();
+    }
+
+    private static void init() {
+        for (int i = 0; i < 10; i++) {
+            container.add(i);
+        }
+    }
+
+
+}
